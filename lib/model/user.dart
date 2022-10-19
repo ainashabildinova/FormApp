41a8ad7a067e0a24616c293a61814ce99897d798
Ainash <114551971+ainashabildinova@users.noreply.github.com>
2022-10-19 09:15:42 +0600

    Update user.dart

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,9 +1,9 @@
 class User {
   late String name;
-  late String id;
   late String phone;
   late String email;
   late String country;
-  late String gender;
   late String story;
+  late String gender;
+  late String id;
 }
